85932d85017ffcccf49288d36c2106a254763008
Leon Do <leondo123@gmail.com>
2017-04-12 23:02:33 -0400

    personal added

--- nodejs voting frontEnd/server.js
+++ nodejs voting frontEnd/server.js
@@ -48,8 +48,9 @@ var contractInstance = VotingContract.at('0x2B79dFf7cD509365B664ED7A1d4C06Ee04c1
 function voteBitch(votingArray){
 
     //unlock the account
-    //web3.personal.unlockAccount("0x26F14FaBCa8A3c18E12F0B83Ac991F5dC7A7A7f3", "password")
+    web3.personal.unlockAccount('0x26F14FaBCa8A3c18E12F0B83Ac991F5dC7A7A7f3', "password", 99999)
 
+    
     // contractInstance.totalVotes(
     //     votingArray, 
     //     {data: contract['<stdin>:myContract'].code, 
